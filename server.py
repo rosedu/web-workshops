e97b86355f5f8050d279973ab6f47ea7a4f1fed8
Alex Morega <alex@grep.ro>
2011-12-01 12:26:03 +0200

    save submitted data to a file

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
+import os
+import json
 import flask
 
 
+app_dir = os.path.dirname(__file__)
 app = flask.Flask(__name__)
+app.config['SIGNUP_FILE'] = os.path.join(app_dir, 'signup.yaml')
 
 
 @app.route("/")
@@ -15,7 +19,9 @@ def index():
 
 @app.route("/signup", methods=['POST'])
 def signup():
-    print flask.request.form
+    with open(app.config['SIGNUP_FILE'], 'ab') as f:
+        json.dump(flask.request.form.to_dict(), f)
+        f.write('\n---\n')
     return "thank you"
 
 
