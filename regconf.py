9ba0620d88a6772287ac6928a5b6ae4e05ca9481
Dan Filimon <dangeorge.filimon@gmail.com>
2011-12-02 21:33:10 +0200

    made form more configurable

--- regconf.py
+++ regconf.py
@@ -1,5 +1,52 @@
+# -*- coding: utf-8 -*-
 # Regman's config module
 
-TOPICS_FILE = 'topics.txt'
-SIGNUP_FILE = 'signups.yaml'
+def get_lines(file_path):
+    with open(file_path, 'r') as f:
+        return f.readlines()
 
+class BaseConfig(object):
+    # Store registered users and prefereneces here; could be a DB later?
+    SIGNUP_FILE = None
+
+    # Greeting banner
+    GREETING = """ Workshop-uri ținute de oameni tineri și deschiși care lucrează
+în domeniul web."""
+
+    # Lines and columns for matrix of checkboxes
+    # Can be workshop wishlist, date&time, volunteering form
+    LINES = None
+    COLS = None
+
+    # Profile for an attendant as well as contact info
+    PROFILES = {'school' : [u'Școală', u'Clasă'],
+                'college' : ['Facultate', 'An'],
+                'employed' : ['Companie', u'Funcție']}
+    CONTACT = {'email' : u'Adresă de e-mail'}
+
+    # Follow up message
+    FOLLOWUP = u'Contactați-ne la <a href="mailto:webdev@rosedu.org">webdev.rosedu.org</a>'
+
+class StudentConfig(BaseConfig):
+    LINES = ['HTML',
+             'CSS',
+             'JavaScript',
+             'MVC',
+             'Securitate',
+             'REST',
+             'Caching',
+             u'Scaling',
+             'Deployment',
+             'Analytics']
+    COLS = [u'Știu', u'Învăț']
+
+class InstructorConfig(StudentConfig):
+    StudentConfig.COLS.append(u'Predau')
+    StudentConfig.CONTACT.update({'phone' : u'Număr de telefon'})
+
+class DateTimeConfig(BaseConfig):
+    GREETING = 'Analytics 2'
+    LINES = [u'5 decembrie (Luni)',
+             u'6 decembrie (Marți)',
+             u'10 decembrie (Sâmbătă)']
+    COLS = ['16', '18', '20']
--- regman.py
+++ regman.py
@@ -2,26 +2,33 @@ import os
 import json
 import re
 import flask
+
+# Config module
 import regconf
 
 app_dir = os.path.dirname(__file__)
 app = flask.Flask(__name__)
-app.config.from_object(regconf)
+app.config.from_object('regconf.InstructorConfig')
 
 @app.route("/")
 def index():
-    with open(app.config['TOPICS_FILE'], 'r') as topics:
-        return flask.render_template('index.html', topics=topics.readlines())
+    config = app.config
+    return flask.render_template('index.html', lines=config['LINES'],
+                                 cols=config['COLS'])
 
 @app.route("/signup", methods=['POST'])
 def signup():
     form = flask.request.form
 
+    # Why are we doing this twice?
     if re.match(r'^\S+@\S+\.\S+$', form['email']) is None:
         return "Missing email address :("
 
     with open(app.config['SIGNUP_FILE'], 'ab') as f:
-        json.dump(form.to_dict(), f)
+        # Do NOT flatten!
+        # All the checkboxes are named form-topic-{vreau,stiu} and flattening
+        # would only keep the last preference"
+        json.dump(form.to_dict(flat=False), f)
         f.write('\n---\n')
 
     return "Thanks for signing up! We'll keep you posted ;)"
